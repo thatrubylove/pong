bee5ff79e075ae84597861426d74cf4ecfb37df3
thatrubylove <thatrubylove@gmail.com>
2013-12-03 10:37:16 -0800

    gemified pong

---
+++ lib/pong.rb
@@ -0,0 +1,13 @@
+require "pong/version"
+
+Hasu.load "pong/game.rb"
+Hasu.load "pong/ball.rb"
+Hasu.load "pong/paddle.rb"
+
+module Pong
+  extend self
+
+  def start
+    Game.run
+  end
+end
---
+++ lib/pong/paddle.rb
@@ -0,0 +1,73 @@
+module Pong
+  class Paddle
+    WIDTH = 16
+    HEIGHT = 96
+    SPEED = 6
+
+    attr_reader :side, :y, :ai
+    alias ai? ai
+    def initialize(side, ai=false)
+      @ai = ai
+      @side = side
+      @y = Game::HEIGHT/2
+    end
+
+    def ai_move!(ball)
+      if (y - ball.y).abs > SPEED
+        if y > ball.y
+          up!
+        else
+          down!
+        end
+      end
+    end
+
+    def x1
+      case side
+      when :left
+        0
+      when :right
+        Game::WIDTH - WIDTH
+      end
+    end
+
+    def x2
+      x1 + WIDTH
+    end
+
+    def y1
+      y - HEIGHT/2
+    end
+
+    def y2
+      y1 + HEIGHT
+    end
+
+    def draw(window)
+      color = Gosu::Color::WHITE
+
+      window.draw_quad(
+        x1, y1, color,
+        x1, y2, color,
+        x2, y2, color,
+        x2, y1, color,
+      )
+    end
+
+    def up!
+      @y -= SPEED
+
+      if y1 < 0
+        @y = HEIGHT/2
+      end
+    end
+
+    def down!
+      @y += SPEED
+
+      if y2 > Game::HEIGHT
+       @y = Game::HEIGHT - HEIGHT/2
+      end
+    end
+  end
+end
